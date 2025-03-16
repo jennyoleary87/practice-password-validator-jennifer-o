3f6b76ea8d77b931d77d872ea16f9559e6e1400b
jennyo <jennifer.oleary.10@gmail.com>
2025-03-16 16:35:02 -0500

    complete assignment

--- main.js
+++ main.js
@@ -20,3 +20,54 @@ know they have been successful
 ○ If the password does NOT meet the requirements, a statement to let
 the user know their password does not meet the requirements
 */
+
+const readlineSync = require('readline-sync');
+
+function validatePassword() {
+    let isValid = false;
+    let password;
+
+    // Use readlineSync.question() to prompt user for input    
+    // prompt user to enter a password    
+    do {
+        password = readlineSync.question('Enter password: ');
+
+        let isLongEnough = false
+        let hasUpperCase = false;
+        let hasNumber = false;
+
+        // must be at least 8 characters long
+        if (password.length >= 8) {
+            isLongEnough = true;
+        }
+
+        // check for uppercase and number
+        for (let i = 0; i < password.length; i++) {
+            const char = password.charAt(i);
+
+            // must contain at least one number
+            if (!isNaN(parseInt(char))) {
+                hasNumber = true;
+            }
+
+            // must contain at least one uppercase letter
+            if (char === char.toUpperCase() && char !== char.toLowerCase()) {
+                hasUpperCase = true;
+            }
+        }
+
+        isValid = isLongEnough && hasNumber && hasUpperCase;
+
+        if (isValid) {
+            console.log("Success! Your password meets all requirements.");
+        } else {
+            console.log("Password does not meet the requirements:");
+            if (!isLongEnough) console.log("- Password must be at least 8 characters long");
+            if (!hasUpperCase) console.log("- Password must contain at least one uppercase letter");
+            if (!hasNumber) console.log("- Password must contain at least one number");
+            console.log("Please try again.");
+        }
+    } while (!isValid);
+}
+
+validatePassword();
